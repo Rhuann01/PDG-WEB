3b0321c81e9dc5aa016aae42f688a3c9c72abfce
Rhuann01 <rhuann2006@gmail.com>
2026-05-04 20:43:32 -0300

    chore: config details

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import { Card } from "@/app/_components/ui/card";
-import { Map, MapControls } from "./_components/ui/map";
+import { Map } from "./_components/ui/map";
 
 export default function Home() {
   return (
     <Card className="h-screen p-0 overflow-hidden">
-      <Map center={[-74.006, 40.7128]} zoom={11}>
-        <MapControls />
-      </Map>
+      <Map center={[-43.3561, -4.8589]} zoom={13} theme={"light"}></Map>
     </Card>
   );
 }
